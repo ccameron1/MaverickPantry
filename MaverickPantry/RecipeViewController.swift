66baf54a8bf82a92794c643b4fcbb6cc86f55dc2
Akroatis <patrick.stacey2000@gmail.com>
2019-06-19 14:03:47 -0500

    Added View controller files to all of our current storyboards.

---
+++ MaverickPantry/RecipeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecipeViewController.swift
+//  MaverickPantry
+//
+//  Created by Patrick Stacey-Vargus on 6/19/19.
+//  Copyright © 2019 Carly Cameron. All rights reserved.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

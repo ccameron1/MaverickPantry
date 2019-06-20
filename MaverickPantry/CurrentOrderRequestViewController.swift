6dbb083e064073f9e569f00a327afac0e536eb71
Justine Linscott <justinelinscott@unomaha.edu>
2019-06-20 10:21:23 -0500

    request tableview

--- MaverickPantry/CurrentOrderRequestViewController.swift
+++ MaverickPantry/CurrentOrderRequestViewController.swift
@@ -8,24 +8,38 @@
 
 import UIKit
 
-class CurrentOrderRequestViewController: UIViewController {
-
+class CurrentOrderRequestViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var foodGroup : [String] = ["Protein", "Vegetables", "Fruits", "Grain", "Additional Food Products", "Miscellaneous Products", "Personal Hygiene Products"]
+    
+    //var items : [] = []
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Request Form"
 
-        // Do any additional setup after loading the view.
+    }
+
+//tableView Functions
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return foodGroup.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RcellID")
+        let foodType = foodGroup[indexPath.row]
+        cell?.textLabel!.text = foodType
+        return cell!
     }
-    */
+    
+//    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+//        return nil
+//    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
 
 }

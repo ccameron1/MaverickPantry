4f56ecd8f6cba31294cd615853790854017c7e41
JoeBarkley <joeybarkley100@gmail.com>
2019-06-20 11:25:44 -0500

    Added necessary properties to the request view controller file

--- MaverickPantry/RequestPageViewController.swift
+++ MaverickPantry/RequestPageViewController.swift
@@ -9,6 +9,24 @@
 import UIKit
 
 class RequestPageViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var foodTypes : [[String]] = []
+    var foodTypeNames = ["Protein", "Vegetable", "Fruit", "Grain", "Additional Food", "Miscellaneous", "Personal Hygiene"]
+    var foodNames = [""]
+    var proteins = [""]
+    var vegetables = [""]
+    var fruits = [""]
+    var grains = [""]
+    var additional = [""]
+    var miscellaneous = [""]
+    var personalHygiene = [""]
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
     }
@@ -18,24 +36,6 @@ class RequestPageViewController: UIViewController, UITableViewDelegate, UITableV
         return cell
     }
     
-
-    @IBOutlet weak var tableView: UITableView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     @IBAction func submitButtonPressed(_ sender: Any) {
     }
     

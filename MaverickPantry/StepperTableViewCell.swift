151cfcf14ac547f53916f2411061b158b4cfb129
JoeBarkley <joeybarkley100@gmail.com>
2019-06-20 11:04:42 -0500

    Custom class

---
+++ MaverickPantry/StepperTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StepperTableViewCell.swift
+//  MaverickPantry
+//
+//  Created by Joseph Barkley on 6/20/19.
+//  Copyright © 2019 Carly Cameron. All rights reserved.
+//
+
+import UIKit
+
+class StepperTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

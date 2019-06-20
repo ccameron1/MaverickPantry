a303ca9e7f00baa2869df68ba4e7b139eab5fefc
Justine Linscott <justinelinscott@unomaha.edu>
2019-06-20 09:36:00 -0500

    table view for current orders

---
+++ MaverickPantry/CurrentOrderRequestViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CurrentOrderRequestViewController.swift
+//  MaverickPantry
+//
+//  Created by Justine Linscott on 6/20/19.
+//  Copyright © 2019 Carly Cameron. All rights reserved.
+//
+
+import UIKit
+
+class CurrentOrderRequestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Request Form"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MaverickPantry/CurrentOrdersViewController.swift
+++ MaverickPantry/CurrentOrdersViewController.swift
@@ -13,12 +13,17 @@ class CurrentOrdersViewController: UIViewController, UITableViewDataSource, UITa
    // var currentOrder : [] = []
     
     @IBOutlet var tableView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Current Orders"
+        
+//        noOrderLabel.text = "No Orders"
+//        noOrderLabel.isHidden = true
+        
     }
     
-
+// tableview functions
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //return currentOrder.count
         return 1
@@ -26,7 +31,22 @@ class CurrentOrdersViewController: UIViewController, UITableViewDataSource, UITa
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellID")
+        
+        cell?.textLabel!.text = "BirthDate"
+        cell?.detailTextLabel!.text = "Initials"
+//        if currentOrder.count == 0 {
+//              title = "No Current Orders"
+//        } else {
+//        let birthDate = currentOrder[IndexPath.row].birthDate
+//        let initials = currentOrder[IndexPath.row].initals
+//        cell?.textLabel.text = birthDate
+//        cell?.detailTextLabel.text = initials
+//        }
         return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return nil
     }
 
 }

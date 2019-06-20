1ef42e199c68096e63e770714bb4afd9fa79b93c
Justine Linscott <justinelinscott@unomaha.edu>
2019-06-20 14:25:05 -0500

    current table fixed

--- MaverickPantry/CurrentOrderRequestViewController.swift
+++ MaverickPantry/CurrentOrderRequestViewController.swift
@@ -14,10 +14,12 @@ class CurrentOrderRequestViewController: UIViewController, UITableViewDataSource
     
     //var items : [] = []
     
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Request Form"
+        nameLabel.text = "Order For: "
 
     }
 
@@ -34,15 +36,17 @@ class CurrentOrderRequestViewController: UIViewController, UITableViewDataSource
         return cell!
     }
     
-//    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-//        return nil
-//    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return nil
+    }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 85
+        return 75
     }
     
     @IBAction func filledButtonPressed(_ sender: UIButton) {
         perform("")
     }
+    
+    
 }

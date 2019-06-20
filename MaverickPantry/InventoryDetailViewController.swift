e4050ac369046ec77758cd1cac99a9c82bd8a6f0
Gwyneth Semanisin <gesemanisin@gmail.com>
2019-06-20 14:45:46 -0500

    table data won't update til no dummy data

--- MaverickPantry/InventoryDetailViewController.swift
+++ MaverickPantry/InventoryDetailViewController.swift
@@ -60,17 +60,5 @@ class InventoryDetailViewController: UIViewController {
     @IBAction func whenClearButtonPressed(_ sender: UIButton) {
         givenOutLabel.text = "0"
     }
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

d8c87773da709df6b7df65041039134d82d13411
Gwyneth Semanisin <gesemanisin@gmail.com>
2019-06-21 10:20:49 -0500

    dummy food class and functional add button

--- MaverickPantry/InventoryViewController.swift
+++ MaverickPantry/InventoryViewController.swift
@@ -16,17 +16,21 @@ class InventoryViewController: UIViewController, UITableViewDelegate, UITableVie
     var amountGiven = 0
     var foodName = "Applesauce"
     
-    var foods = [[String : Any]]()
+    var foods : [DummyFood] = []
         
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let applesauceDict = ["Food": "Applesauce", "amountLeft": 10, "amountGiven": 50] as [String : Any]
-        let pizzaDict = ["Food": "Pizza", "amountLeft": 43, "amountGiven": 98] as [String : Any]
-        let kaleDict = ["Food": "Kale", "amountLeft": 7, "amountGiven": 3] as [String : Any]
         
-        foods = [applesauceDict,pizzaDict,kaleDict]
+        let applesauce = DummyFood(name: "Applesauce", amountLeft: 42, amountGiven: 75)
+        let pizza = DummyFood(name: "Pizza", amountLeft: 87, amountGiven: 23)
+        let kale = DummyFood(name: "Kale", amountLeft: 100, amountGiven: 0)
+//        let applesauceDict = ["Food": "Applesauce", "amountLeft": 10, "amountGiven": 50] as [String : Any]
+//        let pizzaDict = ["Food": "Pizza", "amountLeft": 43, "amountGiven": 98] as [String : Any]
+//        let kaleDict = ["Food": "Kale", "amountLeft": 7, "amountGiven": 3] as [String : Any]
+//
+        foods = [applesauce,pizza,kale]
         
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -40,10 +44,44 @@ class InventoryViewController: UIViewController, UITableViewDelegate, UITableVie
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CellID") as! UITableViewCell
         let food = foods[indexPath.row]
-        cell.textLabel?.text = "\(food["Food"]!)"
-        cell.detailTextLabel?.text = "Left: \(food["amountLeft"]!)\nGiven Out: \(food["amountGiven"]!)"
+        cell.textLabel?.text = food.name
+        cell.detailTextLabel?.text = "Left: \(food.amountLeft!)\nGiven Out: \(food.amountGiven!)"
         return cell
     }
+    
+    
+    @IBAction func onAddButtonPressed(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Add New Food Item", message: "", preferredStyle: .alert)
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Enter Food Name"
+        }
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Enter Current Amount"
+        }
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Enter Amount Given Out"
+        }
+        let saveAction = UIAlertAction(title: "Save", style: UIAlertAction.Style.default, handler: { alert -> Void in
+            let firstTextField = alertController.textFields![0] as UITextField
+            let secondTextField = alertController.textFields![1] as UITextField
+            let thirdTextField = alertController.textFields![2] as UITextField
+            
+            let name = firstTextField.text!
+            let amountLeft = Int(secondTextField.text!)
+            let amountGiven = Int(thirdTextField.text!)
+            self.foods.append(DummyFood(name: name, amountLeft: amountLeft!, amountGiven: amountGiven!))
+            self.tableView.reloadData()
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: {
+            (action : UIAlertAction!) -> Void in })
+        
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
     
     
     // MARK: - Navigation

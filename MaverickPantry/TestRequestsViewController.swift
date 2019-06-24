5f1104125fb5357256fd9969416d55247797dca2
Akroatis <patrick.stacey2000@gmail.com>
2019-06-24 13:19:08 -0500

    Still working on getting images of selected items

--- MaverickPantry/TestRequestsViewController.swift
+++ MaverickPantry/TestRequestsViewController.swift
@@ -23,7 +23,13 @@ class TestRequestsViewController: UIViewController, UICollectionViewDelegate, UI
     
     var foodTypeNames = ["Protein", "Vegetable", "Fruit", "Grain", "Additional Food", "Miscellaneous", "Personal Hygiene"]
     
-    let imageArray : [UIImage] = [UIImage.init(named: "eggs")!]
+    var imageArray : [[UIImage]] = []
+    
+    var tab1Images : [UIImage] = [UIImage.init(named: "eggs")!, UIImage.init(named: "MisoEggplant")!, UIImage.init(named: "eggs")!, UIImage.init(named: "MisoEggplant")!, UIImage.init(named: "eggs")!]
+    var tab2Images : [UIImage] = [UIImage.init(named: "eggs")!, UIImage.init(named: "MisoEggplant")!, UIImage.init(named: "eggs")!]
+    var tab3Images : [UIImage] = [UIImage.init(named: "eggs")!, UIImage.init(named: "MisoEggplant")!]
+    var tab4Images : [UIImage] = [UIImage.init(named: "eggs")!, UIImage.init(named: "MisoEggplant")!, UIImage.init(named: "eggs")!, UIImage.init(named: "MisoEggplant")!, UIImage.init(named: "eggs")!, UIImage.init(named: "MisoEggplant")!, UIImage.init(named: "eggs")!]
+    var tab5Images : [UIImage] = [UIImage.init(named: "eggs")!, UIImage.init(named: "MisoEggplant")!, UIImage.init(named: "eggs")!, UIImage.init(named: "MisoEggplant")!, UIImage.init(named: "eggs")!, UIImage.init(named: "MisoEggplant")!]
     
     var selectedItems: [String] = []
     var globalFoodArr: [String] = []
@@ -46,6 +52,7 @@ class TestRequestsViewController: UIViewController, UICollectionViewDelegate, UI
         // Initialize the collection views, set the desired frames
         globalFoodArr = proteins
         tabLables = [tab1Lables, tab2Lables, tab3Lables, tab4Lables, tab5Lables]
+        imageArray = [tab1Images, tab2Images, tab3Images, tab4Images, tab5Images]
     }
     
     
@@ -57,7 +64,8 @@ class TestRequestsViewController: UIViewController, UICollectionViewDelegate, UI
             
             cellA.myLabel.text = selectedItems[indexPath.item]
             cellA.imageView.setRounded()
-            cellA.imageView.image = imageArray.first
+            cellA.imageView.image = imageArray.first?.first
+            //cellA.imageView.image = imageArray.first
             return cellA
             
         } else if collectionView == collectionViewB {
@@ -71,8 +79,9 @@ class TestRequestsViewController: UIViewController, UICollectionViewDelegate, UI
             }
             
             cellB.itemNumberLable.text = tabLables[tabSelected][indexPath.item]
+            cellB.imageView.image = imageArray[tabSelected][indexPath.item]
             cellB.imageView.setRounded()
-            cellB.imageView.image = imageArray.first
+            //cellB.imageView.image = imageArray.first
             
             
             cellB.btnTapAction = {

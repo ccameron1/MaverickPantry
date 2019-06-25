603bf666ed590d9502214cc97843957f87abc1d6
Carly Cameron <carlycameron01@gmail.com>
2019-06-25 15:25:13 -0500

    personal hygine

--- MaverickPantry/TestRequestsViewController.swift
+++ MaverickPantry/TestRequestsViewController.swift
@@ -37,7 +37,7 @@ class TestRequestsViewController: UIViewController, UICollectionViewDelegate, UI
     var tab4Images : [UIImage] = [UIImage.init(named: "Granola")!, UIImage.init(named: "ShellsAndCheese")!, UIImage.init(named: "Cheerios")!, UIImage.init(named: "Oatmeal")!, UIImage.init(named: "Spaghetti")!, UIImage.init(named: "BrownRice")!, UIImage.init(named: "WhiteRice")!]
     var tab5Images : [UIImage] = [UIImage.init(named: "Peppercorns")!, UIImage.init(named: "Marinara")!, UIImage.init(named: "Alfredo")!, UIImage.init(named: "TomatoSoup")!, UIImage.init(named: "LentilSoup")!, UIImage.init(named: "ChickenNoodle")!]
     var tab6Images : [UIImage] = [UIImage.init(named: "ToiletPaper")!, UIImage.init(named: "PaperTowels")!]
-    var tab7Images : [UIImage] = []
+    var tab7Images : [UIImage] = [UIImage.init(named: "Peppercorns")!, UIImage.init(named: "Marinara")!, UIImage.init(named: "Alfredo")!, UIImage.init(named: "TomatoSoup")!, UIImage.init(named: "LentilSoup")!, UIImage.init(named: "ChickenNoodle")!]
     
     var selectedItems: [String] = []
     var globalFoodArr: [String] = []
@@ -53,16 +53,19 @@ class TestRequestsViewController: UIViewController, UICollectionViewDelegate, UI
     
     var tabLables : [[String]] = []
     
+    var count = 0
+    
     @IBOutlet weak var totalItemsSelectedLabel: UILabel!
     @IBOutlet weak var collectionViewA: UICollectionView!
     @IBOutlet weak var collectionViewB: UICollectionView!
     @IBOutlet weak var collectionViewC: UICollectionView!
+    @IBOutlet weak var totalFoodItemsSelectedLabel: UILabel!
     
     override func viewDidLoad() {
         // Initialize the collection views, set the desired frames
         globalFoodArr = proteins
-        tabLables = [tab1Lables, tab2Lables, tab3Lables, tab4Lables, tab5Lables, tab6Labels]
-        imageArray = [tab1Images, tab2Images, tab3Images, tab4Images, tab5Images, tab6Images]
+        tabLables = [tab1Lables, tab2Lables, tab3Lables, tab4Lables, tab5Lables, tab6Labels, tab7Labels]
+        imageArray = [tab1Images, tab2Images, tab3Images, tab4Images, tab5Images, tab6Images, tab7Images]
     }
     
     
@@ -103,24 +106,36 @@ class TestRequestsViewController: UIViewController, UICollectionViewDelegate, UI
                 () in
                 print("Edit tapped in cell", indexPath)
                 // update lable and add to correct lable array
-                 let num = Int(cellB.itemNumberLable!.text!)
-                if num! >= 0 && num! < 10 && self.selectedItems.count < 10 {
-                   
-                    cellB.itemNumberLable.text = "\(num! + 1)"
-                    self.tabLables[self.tabSelected][indexPath.row] = cellB.itemNumberLable.text!
+                let num = Int(cellB.itemNumberLable!.text!)
+                var full = false
                 
+                if self.tabSelected != 6 {
+                    if self.count == 10 {
+                        full = true
+                    } else {
+                        self.count = self.count + 1
+                    }
+                }
+                
+                if num! >= 0 && num! < 10 && self.count <= 10 && !full {
+                   
+                        cellB.itemNumberLable.text = "\(num! + 1)"
+                        self.tabLables[self.tabSelected][indexPath.row] = cellB.itemNumberLable.text!
+                    
+                    
                 }
                 
                 if Int(cellB.itemNumberLable.text!)! > 0 {
                     cellB.imageView.layer.borderColor = UIColor.red.cgColor
                 }
                 
-                if self.selectedItems.count < 10 {
+                if self.count <= 10 && !full {
                     self.selectedItems.append(self.globalFoodArr[indexPath.row])
+                    self.totalFoodItemsSelectedLabel.text = "\(self.count)"
                     self.totalItemsSelectedLabel.text = "\(self.selectedItems.count)"
                     self.selectedItemImages.append(cellB.imageView.image!)
-//                    let lastItemIndex = self.collectionViewA.
-//                    collectionView?.scrollToItemAtIndexPath(lastItemIndex, atScrollPosition: .Bottom, animated: true)
+                    //                    let lastItemIndex = self.collectionViewA.
+                    //                    collectionView?.scrollToItemAtIndexPath(lastItemIndex, atScrollPosition: .Bottom, animated: true)
                     self.collectionViewA.reloadData()
                 } else {
                     
@@ -128,7 +143,7 @@ class TestRequestsViewController: UIViewController, UICollectionViewDelegate, UI
                     let alertAction = UIAlertAction.init(title: "Okay", style: .default, handler: nil)
                     alertController.addAction(alertAction)
                     self.present(alertController, animated: false)
-//                    cellB.itemNumberLable.text = "\(num!)"
+                    //                    cellB.itemNumberLable.text = "\(num!)"
                     print("Already have 10 items")
                 }
             }
@@ -137,11 +152,21 @@ class TestRequestsViewController: UIViewController, UICollectionViewDelegate, UI
                 () in
                 print("subtract", indexPath)
                 let num = Int(cellB.itemNumberLable!.text!)
+                var empty = false
+                if self.tabSelected != 6 {
+                    if self.count == 0 {
+                        empty = true
+                    } else {
+                        self.count = self.count - 1
+                    }
+                }
                 if num! > 0 && num! <= 10 {
                     
                     cellB.itemNumberLable.text = "\(num! - 1)"
                     self.tabLables[self.tabSelected][indexPath.row] = cellB.itemNumberLable.text!
-
+                    
+                    
+                    
                 }
                 
                 
@@ -151,11 +176,12 @@ class TestRequestsViewController: UIViewController, UICollectionViewDelegate, UI
                     
                 }
                 
-                if self.selectedItems.count > 0 && num! != 0 {
+                if self.count >= 0 && num! != 0 && !empty {
                     
                     let index = self.selectedItems.firstIndex(of: "\(self.globalFoodArr[indexPath.row])")
                     self.selectedItems.remove(at: index!)
                     self.totalItemsSelectedLabel.text = "\(self.selectedItems.count)"
+                    self.totalFoodItemsSelectedLabel.text = "\(self.count)"
                     self.selectedItemImages.remove(at: index!)
                     self.collectionViewA.reloadData()
                     
@@ -166,7 +192,7 @@ class TestRequestsViewController: UIViewController, UICollectionViewDelegate, UI
                     alertController.addAction(alertAction)
                     self.present(alertController, animated: false)
                     
-//                    cellB.itemNumberLable.text = "\(num!)"
+                    //                    cellB.itemNumberLable.text = "\(num!)"
                     print("There is nothing to subtract")
                 } else if num! == 0 {
                     
@@ -232,6 +258,8 @@ class TestRequestsViewController: UIViewController, UICollectionViewDelegate, UI
                 globalFoodArr = additional
             case 5:
                 globalFoodArr = miscellaneous
+            case 6:
+                globalFoodArr = personalHygiene
             default:
                 print("not added yet")
             }
@@ -261,6 +289,7 @@ class TestRequestsViewController: UIViewController, UICollectionViewDelegate, UI
                 
             }
         }
+        count = 0
         selectedItems = []
         collectionViewA.reloadData()
         totalItemsSelectedLabel.text = ""
@@ -295,7 +324,7 @@ extension UIImageView {
         self.layer.masksToBounds = true
         
         self.layer.borderWidth = 2
-
+        
         //self.layer.borderColor = UIColor.lightGray.cgColor
     }
     

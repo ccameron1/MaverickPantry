e7b07258edd472f9c644f4ef4ba09f1e6321892a
Justine Linscott <justinelinscott@unomaha.edu>
2019-08-07 14:35:24 -0500

    finished hardcoding in recipes

--- MaverickPantry/RecipeViewController.swift
+++ MaverickPantry/RecipeViewController.swift
@@ -13,8 +13,8 @@ class RecipeViewController: UIViewController, UICollectionViewDelegate, UICollec
     @IBOutlet weak var collectionView: UICollectionView!
     
     var recipes : [String] = ["pasta", "pizza", "soup", "eggs"]
-    var recipeName : [String] = ["Tuna Patties", "Chicken Casserole", "Chili", "Seven Can Chicken Tortilla Soup"]
-    var time : [String] = ["1 hour and 15 minutes", "45 minutes", "50 minutes", "15 minutes"]
+    var recipeName : [String] = ["Chili", "Seven Can Chicken Tortilla Soup", "Peanut Butter Quinoa Balls", "Vegan Chickpea Meatballs", "Black Bean Burgers", "Baked Tortilla Chips"]
+    var time : [String] = ["50 minutes", "15 minutes", "50 minutes", "40 minutes", "40 minutes", "30 minutes"]
     var indexPath: IndexPath?
     
     override func viewDidLoad() {
@@ -23,15 +23,15 @@ class RecipeViewController: UIViewController, UICollectionViewDelegate, UICollec
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return recipes.count
+        return recipeName.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath) as! RecipeCollectionViewCell
         let time = self.time[indexPath.row]
-        let list = recipes[indexPath.row]
+//        let list = recipes[indexPath.row]
         let name = recipeName[indexPath.row]
-        cell.imageView.image = UIImage(named: list)
+        cell.imageView.image = UIImage(named: "eggs")
         cell.foodLabel.text = name
         cell.timeLabel.text = time
         cell.imageView.addShadow()
@@ -45,5 +45,7 @@ class RecipeViewController: UIViewController, UICollectionViewDelegate, UICollec
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let dvc = segue.destination as! IndividualRecipeViewController
+        dvc.name = recipeName[(indexPath?.row)!]
+        dvc.row = indexPath!.row
     }
 }

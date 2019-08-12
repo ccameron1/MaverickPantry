fa636d2fe883e2d6c1acdbe55da73a20ae1cc7be
Justine Linscott <justinelinscott@unomaha.edu>
2019-08-12 18:01:27 -0500

    firebase recipes

---
+++ MaverickPantry/Recipe.swift
@@ -0,0 +1,32 @@
+//
+//  Recipe.swift
+//  MaverickPantry
+//
+//  Created by Justine Linscott on 8/12/19.
+//  Copyright © 2019 Carly Cameron. All rights reserved.
+//
+
+import Foundation
+
+class Recipe {
+    var recipeName : String?
+    var ingredients : [String]?
+    var recipeTime  : String?
+    var cookTime : String?
+    var image : String?
+    var recipeDescription : String?
+    var recipeDirections : String?
+    var recipeServing : String?
+    
+    init(recipeName: String, ingredients: [String], recipeTime: String, cookTime: String, image: String, recipeDescription: String, recipeDirections: String, recipeServing: String) {
+        self.recipeName = recipeName
+        self.ingredients = ingredients
+        self.recipeTime = recipeTime
+        self.cookTime = cookTime
+        self.image = image
+        self.recipeDescription = recipeDescription
+        self.recipeDirections = recipeDirections
+        self.recipeServing = recipeServing
+    }
+    
+}

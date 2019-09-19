f2c246bc75383bb82928a7a09d9c1f075d608b88
kbrown1504 <keegqbrown@gmail.com>
2019-09-19 13:25:53 -0500

    adding resource deatils

---
+++ MaverickPantry/ResourceDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ResourceDetailViewController.swift
+//  MaverickPantry
+//
+//  Created by Keegan Brown on 9/19/19.
+//  Copyright © 2019 Carly Cameron. All rights reserved.
+//
+
+import UIKit
+
+class ResourceDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- MaverickPantry/ResourcesViewController.swift
+++ MaverickPantry/ResourcesViewController.swift
@@ -51,11 +51,11 @@ class ResourcesViewController: UIViewController, UITableViewDelegate, UITableVie
         switch selectionController.selectedSegmentIndex{
         case 0:
             cell?.textLabel?.text = onCampusResources[indexPath.row].name
-            cell?.detailTextLabel?.text = onCampusResources[indexPath.row].phoneNumber
+            cell?.detailTextLabel?.text = onCampusResources[indexPath.row].phoneNumber + "\n" + onCampusResources[indexPath.row].address
             cell?.imageView?.image = onCampusResources[indexPath.row].image
         case 1:
             cell?.textLabel?.text = offCampusResources[indexPath.row].name
-            cell?.detailTextLabel?.text = offCampusResources[indexPath.row].phoneNumber
+            cell?.detailTextLabel?.text = offCampusResources[indexPath.row].phoneNumber + "\n" + offCampusResources[indexPath.row].address
             cell?.imageView?.image = offCampusResources[indexPath.row].image
         default:
             break;
@@ -70,7 +70,7 @@ class ResourcesViewController: UIViewController, UITableViewDelegate, UITableVie
     func createAndAddResources(){
         
         //add code for on campus
-        let caps = ResourceItem(name: "CAPS", desc: "Counseling and Psychological Services", image: UIImage(named: "icon2")! , link: "https://www.unomaha.edu/student-life/wellness/counseling-and-psychological-services/index.php", phoneNumber: "(402)554-2409", address: "address goes here")
+        let caps = ResourceItem(name: "CAPS", desc: "Counseling and Psychological Services", image: UIImage(named: "icon2")! , link: "https://www.unomaha.edu/student-life/wellness/counseling-and-psychological-services/index.php", phoneNumber: "(402)554-2409", address: "6323 Maverick Plaza, Omaha, NE 68182")
         onCampusResources.append(caps)
         
         //add code for off campus
